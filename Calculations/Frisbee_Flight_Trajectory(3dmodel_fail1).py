8983c9717823ca11c52b29cfac52001ec3255640
Grebtsew <danwe980@student.liu.se>
2019-09-23 13:16:54 +0200

    Updated for help request

--- Calculations/Frisbee_Flight_Trajectory(3dmodel_fail1).py
+++ Calculations/Frisbee_Flight_Trajectory(3dmodel_fail1).py
@@ -21,10 +21,9 @@ Termology explained:
                  -
 
 Main sources:
-http://scripts.mit.edu/~womens-ult/frisbee_physics.pdf
+http://scripts.mit.edu/~womens-ult/frisbee_physics.pdf (good article with about 2dmodels)
 https://scholarworks.moreheadstate.edu/cgi/viewcontent.cgi?article=1082&context=student_scholarship_posters
-https://morleyfielddgc.files.wordpress.com/2009/04/hummelthesis.pdf
-
+https://morleyfielddgc.files.wordpress.com/2009/04/hummelthesis.pdf (good article with about 3dmodels)
 """
 
 
@@ -49,7 +48,6 @@ Rim_Thickness = 0.012
 Rim_depth = 0.013
 Center_Height = 0.019 # max height
 
-
 CL0 = 0.1 #The lift coefficient at alpha=0.
 CLA = 1.4 #The lift coefficient dependent on alpha
 
@@ -66,28 +64,24 @@ side_AREA = R*2*Center_Height
 Physics functions
 """
 
-def x_axis_velocity(zrot, vx):
+def x_axis_physics(alpha, vx):
     # Calculate the drag coefficient for prantls relationship
-    cd = CD0 + CDA *pow((zrot -ALPHA0)*math.pi/180, 2)
+    cd = CD0 + CDA *pow((alpha -ALPHA0)*math.pi/180, 2)
     # The change invelocity in the x direction , obtained by solving
     # the force equation for deltav. ( The only force present is the drag force)
-    return -RHO*pow(vx,2)*side_AREA*cd*deltaT;
+    return -RHO*pow(vx,2)*above_AREA*cd*deltaT;
 
-def y_axis_velocity(xrot, zrot, vx):
+def y_axis_physics(alpha, vx):
     # Calculation of the lift coefficient using the relationship given by S.A.Hummel.
-    cl = CL0 + CLA *zrot *math.pi/180
+    cl = CL0 + CLA *alpha *math.pi/180
     # The change in velocity in the y direction obtained
     # setting the  net force equal to the sum of the gravitational force and the lift force and solving for deltav
     return (RHO*pow(vx,2)*above_AREA*cl/2/m+g)*deltaT
 
-def z_axis_velocity(xrot,zrot, vx):
-    # Calculation of the lift coefficient using the relationship given by S.A.Hummel.
-    cl = CL0 + CLA *zrot *math.pi/180
-    # The change in velocity in the y direction obtained
-    # setting the  net force equal to the sum of the gravitational force and the lift force and solving for deltav
-    return math.cos(xrot)*(RHO*pow(vx,2)*above_AREA*cl/2/m+g)*deltaT
 
-def x_axis_rot(yrot,xrot, vx):
+def z_axis_physics(beta, vx, vy, vz):
+
+    # Aerodynamic physics
     """
     ROLL
     Crr, Crp = constants, z = Roll = p
@@ -96,10 +90,22 @@ def x_axis_rot(yrot,xrot, vx):
     """
     crr = 0.014
     crp = -0.0055
-    roll = (crr*yrot*math.pi / 180 + crp*xrot*math.pi / 180)* 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
-    return roll*deltaT
+    roll = (crr*vy + crp*vz)* 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
 
-def y_axis_rot(yrot, vx):
+    """
+    PITCH
+    Cm0, Cma, Cmq = constants, x = Pitch  = q
+    Formula:
+    M = (CM0 + Cma*alpha + CMq*q)*1/2*RHO*v^2*AREA*d
+    """
+    CM0 = -0.08
+    CMA = 0.43
+    CMq = - 0.005
+    pitch =(CM0 + CMA*beta*math.pi / 180  + CMq*vx) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
+
+    return roll+pitch
+
+def rotation(rot, vx):
     """
     SPIN DOWN (might be negligible)
     y = spin down = r
@@ -108,41 +114,24 @@ def y_axis_rot(yrot, vx):
     N = (CNR*r)*1/2*RHO*v^2*AREA*d
     """
     CNR = -0.000034
-    spin = (CNR*yrot*math.pi / 180) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
-    return spin*deltaT
+    N = (CNR*rot) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
+    return N
 
-def z_axis_rot(zrot, zrot0, vx):
-    """
-    PITCH
-    Cm0, Cma, Cmq = constants, x = Pitch = vx
-    Formula:
-    M = (CM0 + Cma*alpha + CMq*q)*1/2*RHO*v^2*AREA*d
-    """
-    CM0 = -0.08
-    CMA = 0.43
-    CMq = - 0.005
-    pitch =(CM0 + CMA*(-CL0/CLA)*math.pi / 180  + CMq* zrot*math.pi / 180) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
-    return pitch*deltaT
-
-def simulate(y0, vx0, vy0, vz0, xrot0, yrot0, zrot0, deltaT):
+def simulate(y0, vx0, vy0, vz0, alpha, beta, rot0, deltaT):
 
     print("Simulation Started:")
     print( "Velocity Vector (",vx0, vy0, vz0,") [m/s]" )
     print( "Velocity Vector (",3.6*vx0, 3.6*vy0, 3.6*vz0,") [km/h]" )
-    print( "Rotation Vector (",xrot0, yrot0, zrot0,")")
-    print( "Rotation Speed ", yrot0, "[rad/s]")
+    print( "Rotation Vector (",alpha, 0, beta,")")
+    print( "Rotation Speed ", rot0, "[rad/s]")
 
     x = 0
     y = y0
     z = 0
-
     vx = vx0
     vy = vy0
     vz = vz0
-
-    xrot = xrot0
-    yrot = yrot0
-    zrot = zrot0
+    rot = rot0
 
     loop_count = 0
 
@@ -150,30 +139,20 @@ def simulate(y0, vx0, vy0, vz0, xrot0, yrot0, zrot0, deltaT):
     x_values = []
     y_values = []
     z_values = []
-    xrot_values = []
-    yrot_values = []
-    zrot_values = []
     vx_values = []
     vz_values = []
 
     while (y > 0):
 
-        deltavy = y_axis_velocity(xrot,zrot0, vx)
-        deltavx = x_axis_velocity(zrot0, vx)
-        deltavz = z_axis_velocity(xrot,zrot0, vx)
+        deltavy =y_axis_physics(alpha, vx)
+        deltavx =x_axis_physics(alpha, vx)
+        deltavz =z_axis_physics(beta, vx, vy, vz)
 
-        xrot = x_axis_rot(yrot,xrot, vx)
-        yrot = y_axis_rot(yrot, vx)
-        zrot = z_axis_rot(zrot, zrot0, vx)
-
-    #    xrot = xrot + deltarotx
-    #    yrot = yrot + deltaroty
-    #    zrot = zrot + deltarotz
+        rot = rotation(rot, vx)*deltaT
 
         vx=vx+deltavx
         vy=vy+deltavy
         vz=vz+deltavz
-
         x=x+vx*deltaT
         y=y+vy*deltaT
         z=z+vz*deltaT
@@ -184,17 +163,14 @@ def simulate(y0, vx0, vy0, vz0, xrot0, yrot0, zrot0, deltaT):
             z_values.append(z)
             vz_values.append(vz)
             vx_values.append(vx)
-            xrot_values.append(xrot)
-            yrot_values.append(yrot)
-            zrot_values.append(zrot)
 
         loop_count+=1
 
     print("Done, Number of calculation iterations: ", loop_count)
-    plot_graphs(x_values, y_values, z_values, vz_values, vx_values, xrot_values, yrot_values, zrot_values)
+    plot_graphs(x_values, y_values, z_values, vz_values, vx_values)
 
-def plot_graphs(x_values, y_values, z_values, vz_values, vx_values, xrot_values, yrot_values, zrot_values):
-    fig, axs = plt.subplots(5)
+def plot_graphs(x_values, y_values, z_values, vz_values, vx_values):
+    fig, axs = plt.subplots(2)
 
     axs[0].set_title('Frisbee Flight Trajectory')
     axs[0].set_xlabel('X distance [m] ')
@@ -207,37 +183,20 @@ def plot_graphs(x_values, y_values, z_values, vz_values, vx_values, xrot_values,
     axs[1].plot(z_values, x_values, '-b', label= "z")
     axs[1].plot(vz_values,'-r', label= "vz")
 
-    axs[2].set_xlabel('xrot')
-    axs[2].set_ylabel('X distance [m]')
-    axs[2].plot(xrot_values, x_values, '-b', label= "xrot")
-
-    axs[3].set_xlabel('yrot')
-    axs[3].set_ylabel('X distance [m]')
-    axs[3].plot(yrot_values, x_values, '-b', label= "yrot")
-
-    axs[4].set_xlabel('zrot')
-    axs[4].set_ylabel('X distance [m]')
-    axs[4].plot(zrot_values, x_values, '-b', label= "zrot")
-
-
     axs[0].legend(loc="upper right")
     axs[1].legend(loc="upper right")
-    axs[2].legend(loc="upper right")
-    axs[3].legend(loc="upper right")
-    axs[4].legend(loc="upper right")
 
-
-    plt.subplots_adjust(hspace = 2  )
+    plt.subplots_adjust(hspace = 0.5  )
     plt.show()
 
 if __name__ == "__main__":
     y0 = 1 # m above ground
-    vx0 = 14 # m/s x axis
+    vx0 = 27 # m/s x axis
     vy0 = 0 # m/s y axis
     vz0 = 0 # m/s in z axis
-    yrot0 = 50 # rotation radian per second
-    xrot0 = 0 # rotation radian per second
-    zrot0 = 7.5 # rotation radian per second
+    rot0 = math.pi # rotation radian per second
+    alpha = 7.5 # alpha angle degrees (z axis)
+    beta = 0  # beta angle degrees
     deltaT = 0.1 # number of calculation intervalls
 
-    simulate(y0, vx0, vy0, vz0,  xrot0, yrot0, zrot0, deltaT)
+    simulate(y0, vx0, vy0, vz0, alpha, beta, rot0, deltaT)
--- Calculations/Frisbee_Flight_Trajectory(3dmodel_fail2).py
+++ Calculations/Frisbee_Flight_Trajectory(3dmodel_fail2).py
@@ -9,8 +9,6 @@ Termology explained:
     y - axis are height
     z - axis are sides
 
-    *alpha is then the angle between x-axis and the outgoing direction
-    *beta is the angle in z-axis
 
  Above AREA
 [-----------]
@@ -20,10 +18,11 @@ Termology explained:
  \|       |/     |
                  -
 
+
 Main sources:
-http://scripts.mit.edu/~womens-ult/frisbee_physics.pdf
+http://scripts.mit.edu/~womens-ult/frisbee_physics.pdf (good article about 2dmodels)
 https://scholarworks.moreheadstate.edu/cgi/viewcontent.cgi?article=1082&context=student_scholarship_posters
-https://morleyfielddgc.files.wordpress.com/2009/04/hummelthesis.pdf
+https://morleyfielddgc.files.wordpress.com/2009/04/hummelthesis.pdf (good article about 3dmodels)
 
 """
 
@@ -49,6 +48,7 @@ Rim_Thickness = 0.012
 Rim_depth = 0.013
 Center_Height = 0.019 # max height
 
+
 CL0 = 0.1 #The lift coefficient at alpha=0.
 CLA = 1.4 #The lift coefficient dependent on alpha
 
@@ -65,24 +65,28 @@ side_AREA = R*2*Center_Height
 Physics functions
 """
 
-def x_axis_physics(alpha, vx):
+def x_axis_velocity(zrot, vx):
     # Calculate the drag coefficient for prantls relationship
-    cd = CD0 + CDA *pow((alpha -ALPHA0)*math.pi/180, 2)
+    cd = CD0 + CDA *pow((zrot -ALPHA0)*math.pi/180, 2)
     # The change invelocity in the x direction , obtained by solving
     # the force equation for deltav. ( The only force present is the drag force)
-    return -RHO*pow(vx,2)*above_AREA*cd*deltaT;
+    return -RHO*pow(vx,2)*side_AREA*cd*deltaT;
 
-def y_axis_physics(alpha, vx):
+def y_axis_velocity(xrot, zrot, vx):
     # Calculation of the lift coefficient using the relationship given by S.A.Hummel.
-    cl = CL0 + CLA *alpha *math.pi/180
+    cl = CL0 + CLA *zrot *math.pi/180
     # The change in velocity in the y direction obtained
     # setting the  net force equal to the sum of the gravitational force and the lift force and solving for deltav
     return (RHO*pow(vx,2)*above_AREA*cl/2/m+g)*deltaT
 
+def z_axis_velocity(xrot,zrot, vx):
+    # Calculation of the lift coefficient using the relationship given by S.A.Hummel.
+    cl = CL0 + CLA *zrot *math.pi/180
+    # The change in velocity in the y direction obtained
+    # setting the  net force equal to the sum of the gravitational force and the lift force and solving for deltav
+    return math.cos(xrot)*(RHO*pow(vx,2)*above_AREA*cl/2/m+g)*deltaT
 
-def z_axis_physics(beta, vx, vy, vz):
-
-    # Aerodynamic physics
+def x_axis_rot(yrot,xrot, vx):
     """
     ROLL
     Crr, Crp = constants, z = Roll = p
@@ -91,22 +95,10 @@ def z_axis_physics(beta, vx, vy, vz):
     """
     crr = 0.014
     crp = -0.0055
-    roll = (crr*vy + crp*vz)* 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
+    roll = (crr*yrot*math.pi / 180 + crp*xrot*math.pi / 180)* 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
+    return roll*deltaT
 
-    """
-    PITCH
-    Cm0, Cma, Cmq = constants, x = Pitch  = q
-    Formula:
-    M = (CM0 + Cma*alpha + CMq*q)*1/2*RHO*v^2*AREA*d
-    """
-    CM0 = -0.08
-    CMA = 0.43
-    CMq = - 0.005
-    pitch =(CM0 + CMA*beta*math.pi / 180  + CMq*vx) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
-
-    return roll+pitch
-
-def rotation(rot, vx):
+def y_axis_rot(yrot, vx):
     """
     SPIN DOWN (might be negligible)
     y = spin down = r
@@ -115,24 +107,41 @@ def rotation(rot, vx):
     N = (CNR*r)*1/2*RHO*v^2*AREA*d
     """
     CNR = -0.000034
-    N = (CNR*rot) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
-    return N
+    spin = (CNR*yrot*math.pi / 180) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
+    return spin*deltaT
 
-def simulate(y0, vx0, vy0, vz0, alpha, beta, rot0, deltaT):
+def z_axis_rot(zrot, zrot0, vx):
+    """
+    PITCH
+    Cm0, Cma, Cmq = constants, x = Pitch = vx
+    Formula:
+    M = (CM0 + Cma*alpha + CMq*q)*1/2*RHO*v^2*AREA*d
+    """
+    CM0 = -0.08
+    CMA = 0.43
+    CMq = - 0.005
+    pitch =(CM0 + CMA*(-CL0/CLA)*math.pi / 180  + CMq* zrot*math.pi / 180) * 1/2 * RHO * pow(vx,2) * above_AREA * 2*R
+    return pitch*deltaT
+
+def simulate(y0, vx0, vy0, vz0, xrot0, yrot0, zrot0, deltaT):
 
     print("Simulation Started:")
     print( "Velocity Vector (",vx0, vy0, vz0,") [m/s]" )
     print( "Velocity Vector (",3.6*vx0, 3.6*vy0, 3.6*vz0,") [km/h]" )
-    print( "Rotation Vector (",alpha, 0, beta,")")
-    print( "Rotation Speed ", rot0, "[rad/s]")
+    print( "Rotation Vector (",xrot0, yrot0, zrot0,")")
+    print( "Rotation Speed ", yrot0, "[rad/s]")
 
     x = 0
     y = y0
     z = 0
+
     vx = vx0
     vy = vy0
     vz = vz0
-    rot = rot0
+
+    xrot = xrot0
+    yrot = yrot0
+    zrot = zrot0
 
     loop_count = 0
 
@@ -140,20 +149,30 @@ def simulate(y0, vx0, vy0, vz0, alpha, beta, rot0, deltaT):
     x_values = []
     y_values = []
     z_values = []
+    xrot_values = []
+    yrot_values = []
+    zrot_values = []
     vx_values = []
     vz_values = []
 
     while (y > 0):
 
-        deltavy =y_axis_physics(alpha, vx)
-        deltavx =x_axis_physics(alpha, vx)
-        deltavz =z_axis_physics(beta, vx, vy, vz)
+        deltavy = y_axis_velocity(xrot,zrot0, vx)
+        deltavx = x_axis_velocity(zrot0, vx)
+        deltavz = z_axis_velocity(xrot,zrot0, vx)
 
-        rot = rotation(rot, vx)*deltaT
+        xrot = x_axis_rot(yrot,xrot, vx)
+        yrot = y_axis_rot(yrot, vx)
+        zrot = z_axis_rot(zrot, zrot0, vx)
+
+    #    xrot = xrot + deltarotx
+    #    yrot = yrot + deltaroty
+    #    zrot = zrot + deltarotz
 
         vx=vx+deltavx
         vy=vy+deltavy
         vz=vz+deltavz
+
         x=x+vx*deltaT
         y=y+vy*deltaT
         z=z+vz*deltaT
@@ -164,14 +183,17 @@ def simulate(y0, vx0, vy0, vz0, alpha, beta, rot0, deltaT):
             z_values.append(z)
             vz_values.append(vz)
             vx_values.append(vx)
+            xrot_values.append(xrot)
+            yrot_values.append(yrot)
+            zrot_values.append(zrot)
 
         loop_count+=1
 
     print("Done, Number of calculation iterations: ", loop_count)
-    plot_graphs(x_values, y_values, z_values, vz_values, vx_values)
+    plot_graphs(x_values, y_values, z_values, vz_values, vx_values, xrot_values, yrot_values, zrot_values)
 
-def plot_graphs(x_values, y_values, z_values, vz_values, vx_values):
-    fig, axs = plt.subplots(2)
+def plot_graphs(x_values, y_values, z_values, vz_values, vx_values, xrot_values, yrot_values, zrot_values):
+    fig, axs = plt.subplots(5)
 
     axs[0].set_title('Frisbee Flight Trajectory')
     axs[0].set_xlabel('X distance [m] ')
@@ -184,20 +206,37 @@ def plot_graphs(x_values, y_values, z_values, vz_values, vx_values):
     axs[1].plot(z_values, x_values, '-b', label= "z")
     axs[1].plot(vz_values,'-r', label= "vz")
 
+    axs[2].set_xlabel('xrot')
+    axs[2].set_ylabel('X distance [m]')
+    axs[2].plot(xrot_values, x_values, '-b', label= "xrot")
+
+    axs[3].set_xlabel('yrot')
+    axs[3].set_ylabel('X distance [m]')
+    axs[3].plot(yrot_values, x_values, '-b', label= "yrot")
+
+    axs[4].set_xlabel('zrot')
+    axs[4].set_ylabel('X distance [m]')
+    axs[4].plot(zrot_values, x_values, '-b', label= "zrot")
+
+
     axs[0].legend(loc="upper right")
     axs[1].legend(loc="upper right")
+    axs[2].legend(loc="upper right")
+    axs[3].legend(loc="upper right")
+    axs[4].legend(loc="upper right")
 
-    plt.subplots_adjust(hspace = 0.5  )
+
+    plt.subplots_adjust(hspace = 2  )
     plt.show()
 
 if __name__ == "__main__":
     y0 = 1 # m above ground
-    vx0 = 27 # m/s x axis
+    vx0 = 14 # m/s x axis
     vy0 = 0 # m/s y axis
     vz0 = 0 # m/s in z axis
-    rot0 = math.pi # rotation radian per second
-    alpha = 7.5 # alpha angle degrees (z axis)
-    beta = 0  # beta angle degrees
-    deltaT = 0.1 # number of calculation intervalls
+    yrot0 = 50 # rotation radian per second
+    xrot0 = 0 # rotation radian per second
+    zrot0 = 7.5 # rotation radian per second
+    deltaT = 0.1 # time intervall
 
-    simulate(y0, vx0, vy0, vz0, alpha, beta, rot0, deltaT)
+    simulate(y0, vx0, vy0, vz0,  xrot0, yrot0, zrot0, deltaT)
